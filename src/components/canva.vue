72cec50c63a5ead4bd40a913b067064893818cd4
wl00795897 <wl00795897@gmail.com>
2023-10-01 02:48:12 +0800

    Human mapping

--- src/components/canva.vue
+++ src/components/canva.vue
@@ -108,7 +108,7 @@ const canva_setup = () => {
   };
 
   // <Sphere>
-  const geometry = new THREE.SphereGeometry(3, 32, 16);
+  const geometry = new THREE.SphereGeometry(4, 32, 16);
   const texture = new THREE.TextureLoader().load(props.imgData);
   const material = new THREE.MeshBasicMaterial({ map: texture });
   sphere = new THREE.Mesh(geometry, material);
@@ -118,7 +118,7 @@ const canva_setup = () => {
   // <GLOBE>
   // https://web.archive.org/web/20120107030109/http://cgafaq.info/wiki/Evenly_distributed_points_on_sphere#Spirals
   let counter = 200000;
-  let rad = 3;
+  let rad = 4;
   let sph = new THREE.Spherical();
 
   let r = 0;
@@ -170,7 +170,7 @@ const canva_setup = () => {
   scene.add(globe);
 
   // <Markers>
-  const markerCount = 30;
+  const markerCount = 8;
   let markerInfo = []; // information on markers
   let gMarker = new THREE.PlaneGeometry();
   let mMarker = new THREE.MeshBasicMaterial({
@@ -213,11 +213,22 @@ const canva_setup = () => {
   });
   mMarker.defines = { USE_UV: " " }; // needed to be set to be able to work with UVs
   let markers = new THREE.InstancedMesh(gMarker, mMarker, markerCount);
-
   let dummy = new THREE.Object3D();
   let phase = [];
+
+let markersData = [
+  { position: new THREE.Vector3(4, 0, 0), mag: "X" },
+  { position: new THREE.Vector3(0, 4, 0), mag: "Y" },
+  { position: new THREE.Vector3(0, 0, 4), mag: "Z" },
+  { position: new THREE.Vector3(-1.75, 2.4, 2.75), mag: "coastal California" },
+  { position: new THREE.Vector3(0.8, 3.85, -0.75), mag: "Barents Sea" },
+  { position: new THREE.Vector3(2.9, 1.5, -2.3), mag: "Red Sea" },
+  { position: new THREE.Vector3(2.85, 0.25, 2.81), mag: "Equatorial Atlantic Ocean" },
+  { position: new THREE.Vector3(3.65, -1.6, -0.47), mag: "Ocean near Kalahari Desert" },
+];
   for (let i = 0; i < markerCount; i++) {
-    dummy.position.randomDirection().setLength(rad + 0.1);
+    //dummy.position.randomDirection().setLength(rad + 0.1);
+    dummy.position.copy(markersData[i].position);
     dummy.lookAt(dummy.position.clone().setLength(rad + 1));
     dummy.updateMatrix();
     markers.setMatrixAt(i, dummy.matrix);
@@ -225,7 +236,7 @@ const canva_setup = () => {
 
     markerInfo.push({
       id: i + 1,
-      mag: THREE.MathUtils.randInt(1, 10),
+      mag: markersData[i].mag,
       crd: dummy.position.clone(),
     });
   }
