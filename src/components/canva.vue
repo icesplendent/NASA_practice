4ab4e05a58b0435658040c26a4c24427a45a9df0
icesplendent <icesplendent@gmail.com>
2023-10-07 09:41:45 +0800

    core: merge finished

--- src/components/canva.vue
+++ src/components/canva.vue
@@ -28,7 +28,7 @@ const container = ref(null);
 import { useWindowSize } from "@vueuse/core";
 
 const { width, height } = useWindowSize();
-// import popup from "./popup.vue";
+import popup from "./popup.vue";
 
 const closeChildPopup = () => {
   console.log("emit");
