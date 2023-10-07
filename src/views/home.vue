a10618b6fc742fb397147570eff46c68dcb91bac
wl00795897 <wl00795897@gmail.com>
2023-10-08 02:30:36 +0800

    home page finished

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,55 +3,43 @@
   <div class="h-screen w-screen overflow-y-auto bg-black text-white font-jura">
     <div class="container mx-auto overflow-x-hidden">
       <div class="h-16"></div>
-      <div class="relative flex flex-col items-center">
-        <div class="pt-20 flex relative px-24 overflow-x-hidden">
-          <div class="basis-1/2">
-            <h1 id="Para1" class="pt-24 pb-3 text-[88px]">frOG</h1>
-            <p class="text-[27px]">
-              Uncle Atuu runs a small ocean garden in Meichu Planet, and
-              everything is fine
-              <br /><br />
-              Until recently...
-            </p>
-          </div>
-          <div class="basis-1/2 overflow-hidden relative left-24 top-0">
-            <img
-              class="h-[610px] w-full object-cover object-center"
-              src="/home1.jpg"
-            />
-          </div>
+      <div class="relative flex flex-col items-center pt-32">
+        <div class="absolute top-6 left-0 px-24">
+          <h1 id="Para1" class="pt-24 pb-3 text-[88px]">frOG</h1>
+          <p class="text-[27px]">
+            Mr.Chu runs a small ocean garden in Meichu Planet,<br />
+            and everything is fine <br /><br />
+            Until recently...
+          </p>
         </div>
+        <img class="w-full object-cover object-center" src="/home1.png" />
+        <img id="Para2" src="../assets/divLine.svg" class="px-24 pb-8 w-full" />
         <a
           v-on:click="
             scrollToParagraph('Para2');
             buttonSound();
           "
+          class="absolute bottom-24"
         >
-          <img
-            src="../assets/home_down.svg"
-            class="absolute bottom-0 animate-bounce"
+          <img src="../assets/home_down.svg" class="animate-bounce"
         /></a>
       </div>
-      <img id="Para2" src="../assets/divLine.svg" class="py-12 px-24 w-full" />
       <div class="relative flex flex-col items-center">
-        <div class="flex relative px-24 pb-8 overflow-x-hidden">
+        <div class="flex w-full px-24 relative pb-8 overflow-x-hidden">
           <div class="basis-1/2 py-20">
             <p class="text-[27px]">
               his marine organisms are getting sick one after another …
             </p>
           </div>
-          <div class="basis-1/2 overflow-hidden relative left-24">
-            <img
-              class="h-[250px] w-full object-cover object-center"
-              src="/home2.jpg"
-            />
+          <div class="basis-1/2 flex justify-end">
+            <img class="scale-90 object-cover object-center" src="/home2.png" />
           </div>
         </div>
-        <div class="bg-cover bg-center bg-[url('/home3.jpg')]">
-          <div class="backdrop-blur-sm px-24 pt-36 pb-72">
+        <div class="bg-cover bg-center bg-[url('/home3.png')]">
+          <div class="px-24 pt-36 pb-72">
             <p class="text-[27px] text-center mx-52">
-              Uncle Atuu finds out that the unbalanced concentration of CO2 and
-              O2 (the former one is too high while the latter one is too low)
+              Mr.Chu finds out that the unbalanced concentration of CO2 and O2
+              (the former one is too high while the latter one is too low)
               <br />is the main cause behind the problem.
             </p>
           </div>
@@ -61,10 +49,9 @@
             scrollToParagraph('Para4');
             buttonSound();
           "
+          class="absolute bottom-12"
         >
-          <img
-            src="../assets/home_down.svg"
-            class="absolute bottom-12 animate-bounce"
+          <img src="../assets/home_down.svg" class="animate-bounce"
         /></a>
       </div>
       <img id="Para4" src="../assets/divLine.svg" class="w-full px-24" />
@@ -92,38 +79,37 @@
         >
           <img
             src="../assets/home_down.svg"
-            class="absolute bottom-12 animate-bounce"
+            class="absolute bottom-12 left-[48%] animate-bounce"
         /></a>
       </div>
       <img id="Para5" src="../assets/divLine.svg" class="w-full px-24" />
-      <div class="px-24 w-full relative">
-        <p class="py-36 text-[27px]">He has 1/4 of carbon cycle manual now.</p>
-        <div class="flex justify-end relative">
-          <img
-            class="h-[510px] w-[1020px] object-cover object-center"
-            src="/home5.jpg"
-          /><img
-            class="h-[275px] w-[355px] absolute object-cover top-[90px] right-[90px] object-center"
-            src="/home6.jpg"
-          />
+      <div class="w-full relative">
+        <div class="px-24 flex">
+          <p class="basis-1/2 pt-48 pb-0 text-[27px]">
+            He has 1/4 of carbon cycle manual now.
+          </p>
+          <img src="/home5.png" class="scale-[0.75] basis-1/2" />
         </div>
+        <div class="h-[300px]"></div>
+        <img src="/home6.png" class="absolute bottom-0 w-full" />
         <div class="flex justify-center">
           <a
             v-on:click="
               scrollToParagraph('Para6');
               buttonSound();
             "
+            class="absolute bottom-8"
           >
-            <img
-              src="../assets/home_down.svg"
-              class="absolute bottom-12 animate-bounce"
+            <img src="../assets/home_down.svg" class="animate-bounce"
           /></a>
         </div>
       </div>
       <img id="Para6" src="../assets/divLine.svg" class="w-full px-24" />
-      <div class="w-full h-[800px] px-24 flex flex-col items-center relative">
-        <p class="pt-44 pb-12 text-[27px] text-center">
-          Follow Uncle on an adventure<br />to search for the 3 missing pieces
+      <div
+        class="w-full px-24 flex flex-col items-center relative overflow-hidden"
+      >
+        <p class="pt-40 pb-24 text-[27px] text-center">
+          Follow Mr.Chu on an adventure<br />to search for the 3 missing pieces
           of his carbon cycle manual! <br /><br />Let’s Go!!!!!!!!!!
         </p>
         <router-link to="/game" class="z-50">
@@ -134,10 +120,12 @@
             Trip to Earth →
           </button>
         </router-link>
-        <div class="absolute flex w-full bottom-0 z-10">
-          <img src="/home7.jpg" class="triangle_L object-cover object-bottom" />
-          <img src="/home8.jpg" class="triangle_R object-cover object-bottom" />
-        </div>
+        <img
+          src="/home_man.png"
+          class="absolute bottom-[-150px] left-[-100px]"
+        />
+        <img src="/home_earth.png" class="absolute bottom-0 right-0" />
+        <div class="h-[500px]"></div>
       </div>
     </div>
   </div>
@@ -161,15 +149,5 @@ import navbar from "../components/nav.vue";
 <style scoped>
 .font-jura {
   font-family: "Jura";
-}
-.triangle_R {
-  width: 50%;
-  height: 600px;
-  clip-path: polygon(100% 0, 0 100%, 100% 100%);
-}
-.triangle_L {
-  width: 50%;
-  height: 600px;
-  clip-path: polygon(0% 0, 0 100%, 100% 100%);
 }
 </style>
---
+++ src/views/howToPlay.vue
@@ -0,0 +1,89 @@
+<template>
+  <navbar />
+  <div
+    id="com"
+    class="hidden md:block h-screen w-screen overflow-y-auto py-24 bg-black"
+  >
+    <div class="relative my-10 h-fit mx-[4rem]">
+      <div class="flex flex-row">
+        <h1 class="hidden md:block text-white text-5xl font-jura">
+          Collections
+        </h1>
+        <div class="flex-grow"></div>
+        <h1 class="hidden md:block text-white text-5xl text-right font-jura">
+          {{ curCollections }} / {{ totalCollections }}
+        </h1>
+      </div>
+      <div class="flex flex-row mt-[1rem]">
+        <p class="text-white font-jura">
+          3 missing pieces of Uncle Atuu's carbon cycle manual !
+        </p>
+        <div class="flex-grow"></div>
+        <div class="progress-container mt-[0.5rem]">
+          <div
+            class="progress-bar"
+            id="progress-bar"
+            :style="'width: ' + progress + '%'"
+          ></div>
+        </div>
+      </div>
+      <hr class="mt-[1rem] mb-[0.5rem]" />
+      <div class="galleries my-[1.5rem] gap-3 grid grid-cols-2 lg:grid-cols-4">
+        <div v-for="(image, index) in imgCollections" :key="index" class="">
+          <div class="gallery bg-blue-700 p-[0.6em] rounded-md">
+            <img :src="image" :alt="image.alt" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useCollectionStore } from "../stores/collection";
+import navbar from "../components/nav.vue";
+import "../style.css";
+
+const useCollection = useCollectionStore();
+
+const totalCollections = 4;
+let curCollections = useCollection.collection.length + 1;
+
+// california - 0
+// barent - 1
+// redsea - 2
+console.log("has collected: ", curCollections);
+// console.log(useCollection.collection[1]);
+
+let imgCollections = [];
+imgCollections.push("/img/collection3.png"); // default: Mr.chu
+useCollection.collection.forEach((idx) => {
+  // console.log('idx = ', idx);
+  // console.log('/img/collection' + idx + '.png');
+  imgCollections.push("/img/collection" + idx + ".png");
+});
+
+// progress bar
+let progress = (curCollections / totalCollections) * 100;
+console.log("progress = ", progress);
+/
+</script>
+
+<style scoped>
+.font-jura {
+  font-family: "Jura";
+}
+
+.progress-container {
+  width: 300px;
+  height: 10px;
+  background-color: #ccc;
+}
+
+.progress-bar {
+  /* width: 0; */
+  /* width: progress; */
+  height: 100%;
+  background-color: #007bff;
+}
+</style>
