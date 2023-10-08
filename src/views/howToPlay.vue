55801a763231a7c0ea3039f7d604236e99e672df
r1407p <buszter0403@gmail.com>
2023-10-08 12:03:14 +0800

    feat: left-right pair

--- src/views/howToPlay.vue
+++ src/views/howToPlay.vue
@@ -15,6 +15,13 @@
         </div>
         <img src='../assets/slash.png' class='bg-cover ml-[52%] w-[16%] h-[16vh] '/>
       </section>
+      <section>
+        <div class='flex flex-row-reverse'>
+          <img src='img/how_to_play/play1.jpg' class='mr-[12%] w-[40%] '/>
+          <div class='bg-white'>iofasdhfpcuamoiefmasoid;jcaowhfsailudhxmuiuwhflidshaf</div>
+        </div>
+        <img src='../assets/slash2.png' class='bg-cover ml-[32%] mr-[52%] w-[16%] h-[16vh] flex flex-row-reverse'/>
+      </section>
     </div>
     
   </div>
